c0d7289c249c8c0c5b5324d6b3e0b457edbb7fe5
LeonPr <leon@masig.co.il>
2024-10-24 15:10:39 +0300

    Add sort by

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -13,25 +13,33 @@ export const bugService = {
     remove,
 }
 
-function query(filters,filterByLabels, sortBy) {
+function query(filters, filterByLabels, sortBy) {
     //  return axios.get(BASE_URL).then(res => res.data)
-    return axios.get(BASE_URL).then(res=>{
-        let data=res.data
-        if(filters.txt){
-            data=data.filter(bug=> bug.title.includes(filters.txt) || bug.description.includes(filters.txt))
+    return axios.get(BASE_URL).then(res => {
+        let data = res.data
+        if (filters.txt) {
+            data = data.filter(bug => bug.title.includes(filters.txt) || bug.description.includes(filters.txt))
         }
         if (filterByLabels.txt) {
-            data = data.filter(bug => 
-                bug.labels.some(label => label.includes(filterByLabels.txt)) 
-            );
-        } 
-
+            data = data.filter(bug =>
+                bug.labels.some(label => label.includes(filterByLabels.txt))
+            )
+        }
+        if (sortBy) {
+            if (sortBy === 'date') {
+                data.sort((a,b)=> new Date(b.createdAt) - new Date(a.createdAt))
+            } else if (sortBy === 'title') {
+                data.sort((a, b) => a.title.localeCompare(b.title))
+            }else if (sortBy === 'severity') {
+                data.sort((a, b) => a.severity -b.severity)
+            }
+        }
         return data
     })
 }
 
 function getById(bugId) {
-     return axios.get(BASE_URL + bugId).then(res => res.data)
+    return axios.get(BASE_URL + bugId).then(res => res.data)
 }
 
 function remove(bugId) {
